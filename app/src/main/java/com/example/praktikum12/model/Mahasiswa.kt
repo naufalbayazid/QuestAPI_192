5c44be098f2905ce000dfbc2ee61a89a6f94717f
naufalbayazid <naufalichwanbayazid@gmail.com>
2024-12-30 11:06:10 +0700

    menambahkan model mahasiswa

--- app/src/main/java/com/example/praktikum12/model/Mahasiswa.kt
+++ app/src/main/java/com/example/praktikum12/model/Mahasiswa.kt
@@ -1,3 +1,20 @@
 package com.example.praktikum12.model
 
-data class Mahasiswa()
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Serializer
+
+@Serializable
+data class Mahasiswa(
+    val nim: String,
+    val nama: String,
+    val alamat: String,
+
+    @SerialName("jenis_kelamin")
+    val jenisKelamin: String,
+
+    val kelas: String,
+    val angkatan: String
+)
+
